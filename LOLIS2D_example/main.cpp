ba7f93762ea9c9ebf29b8ccdcb0c03b0bda2387d
Xwilarg <xwilarg@yahoo.fr>
2019-08-05 17:01:03 +0200

    Add crate to example

--- LOLIS2D_example/main.cpp
+++ LOLIS2D_example/main.cpp
@@ -38,10 +38,20 @@ int main()
 {
 	LOLIS2D::GameManager gm(500, 200, "LOLIS2D Test");
 	LOLIS2D::Scene mainScene("Main");
+
+	// Player
 	LOLIS2D::DynamicGameObject player("Player");
 	player.AddRenderer<LOLIS2D::ColorRenderer>(sf::Color::Red);
 	player.AddScript<PlayerController>();
+
+	// Obstacle
+	LOLIS2D::GameObject crate("Crate");
+	crate.AddRenderer<LOLIS2D::ColorRenderer>(sf::Color(188, 143, 143));  // Brown
+	crate.Move(sf::Vector2f(100.f, 100.f));
+
 	mainScene.AddGameObject(std::move(player));
+	mainScene.AddGameObject(std::move(crate));
+
 	gm.AddScene(std::move(mainScene)); // First added scene is loaded by default
 	gm.Start();
 	return (0);
